aefc88f9b47555600707989758b201a9b8456e15
Niccolo Rojas <niccolo.rojas.ge@gmail.com>
2026-01-28 15:48:09 +0100

    layout

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -2,7 +2,7 @@ function Homepage() {
 
   return (
     <>
-     
+     <h1>benvenuto nella pagina di benvenuto</h1>
     </>
   )
 }
--- src/pages/Whoweare.jsx
+++ src/pages/Whoweare.jsx
@@ -3,7 +3,7 @@ function Whoweare() {
 
   return (
     <>
-     
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat corrupti ipsam expedita accusamus culpa repellat hic nostrum blanditiis officiis doloribus consequatur autem porro, fuga impedit ipsa, vel quo, modi libero?</p>
     </>
   )
 }
